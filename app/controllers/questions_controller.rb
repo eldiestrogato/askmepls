f0e33eb6f0144958bad11938f4a16c024b67c643
eldiestrogato <andy.parkour.2119@gmail.com>
2022-07-06 16:30:10 +0200

    +

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,33 @@
+class QuestionsController < ApplicationController
+
+
+  def create
+    Question.create(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+    )
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def index
+    @questions = Question.all
+  end
+
+  def update
+
+    @question = Question.find(params[:id])
+    @question.update(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+    )
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+  end
+
+end
